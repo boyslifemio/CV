670f848ab2c9445a36d5322b44abc9dd156ee4f3
ryo <ryo.cv.0311@gmail.com>
2016-06-09 16:13:57 +0900

    making another panorama program

---
+++ python3/panorama/sub_dev.py
@@ -0,0 +1,81 @@
+import cv2, numpy
+
+class Image:
+    def __init__(self,name):
+        def make_cambus(img):
+            cambus=numpy.zeros((img.shape[0]*4,img.shape[1]*4,img.shape[2]),numpy.uint8)
+            for i in range(img.shape[1]):
+                for j in range(img.shape[0]):
+                  cambus[j+int(img.shape[0]*4*3/8),i+int(img.shape[1]*4*3/8),:] = img[j,i,:]
+            return cambus
+
+        def resize_image(name):
+            image = cv2.imread(name, cv2.IMREAD_COLOR)
+            image = cv2.resize(image,(int(image.shape[1]*600/3024),int(image.shape[0]*800/4032)))
+            return image
+
+        def calculate(self):
+            detector = cv2.AKAZE_create()
+            keypoints, descriptors = detector.detectAndCompute(self.image,None)
+            return keypoints, descriptors
+
+        self.name = name
+        self.image = make_cambus(resize_image(self.name))
+        self.kp,self.des = calculate(self)
+
+    def show(self):
+        cv2.imshow(self.name, self.image)
+
+class WideImage():
+    def __init__(self,img):
+        def calculate(self):
+            detector = cv2.AKAZE_create()
+            keypoints, descriptors = detector.detectAndCompute(self.image,None)
+            return keypoints, descriptors
+
+        self.name = 'panorama-temp'
+        self.image = img
+        self.kp,self.des = calculate(self)
+
+    def show(self):
+        cv2.imshow(self.name, self.image)
+
+def make_panorama(original1,original2):
+    matcher = cv2.BFMatcher(cv2.NORM_L2,False)
+    matches = matcher.knnMatch(original1.des,original2.des,2)
+    goodmatches = []
+    trainkeys = []
+    querykeys = []
+    maskArray = []
+
+    for i in matches:
+        print(i[0].distance,end='')
+        if i[0].distance < 500:
+            if i[0].distance/i[1].distance < 0.8:
+                print('-->good match')
+                goodmatches.append(i[0])
+                querykeys.append((original1.kp[i[0].queryIdx].pt[0],original1.kp[i[0].queryIdx].pt[1]))
+                trainkeys.append((original2.kp[i[0].trainIdx].pt[0],original2.kp[i[0].trainIdx].pt[1]))
+            else:
+                print('-->not good match')
+        else:
+            print('-->not good match')
+
+    print("-----Calculating Homography-----")
+    H, status = cv2.findHomography(numpy.array(trainkeys),numpy.array(querykeys),cv2.RANSAC)
+    height,width = original1.image.shape[:2]
+
+    mask=numpy.zeros((height, width,1),numpy.uint8)
+    panorama = cv2.warpPerspective(original2.image,H,(width,height))
+    for i in range(width):
+      for j in range(height):
+        if original1.image[j,i].all():
+          if panorama[j,i].all():
+            panorama[j,i] = original1.image[j,i]/2+panorama[j,i]/2
+            mask[j,i]=255
+          else:
+            panorama[j,i] = original1.image[j,i]
+    #cv2.imwrite("mask",mask)
+    cv2.waitKey(0)
+    print("--next--")
+    return panorama
