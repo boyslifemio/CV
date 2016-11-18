f642dd5ac94f8d4d158b5b59d90073c8ea98f03d
ryo <ryo.cv.0311@gmail.com>
2016-11-19 00:05:14 +0900

    modified:   calibration.cc

--- cpp/calibration/calibration.cc
+++ cpp/calibration/calibration.cc
@@ -5,31 +5,60 @@
 using namespace std;
 using namespace cv;
 
-Size boardSize = Size(6, 7)
+Size boardSize = Size(5, 5);
 
-
-Class Calibration{
+class Calibration{
   public:
-    void Calibrate();
-    void save();
     Calibration(int height, int weight);
+    void calibrate(Mat* inputFrame);
+    Mat calibrate2(Mat inputFrame);
+    void calculate(vector<vector<Point3f>>& objectPoints, vector<vector<Point2f>>& imagePoints);
+    void save();
+    int getCalNum();
   private:
-    vector<Mat> 
-
+    int frameHeight, frameWidth;
+    vector<Mat> kArray, distArray;
+    int calNum;
 
 };
 
-Calibration::Calibration(){
-
-
+Calibration::Calibration(int height, int width){
+  frameHeight = height; 
+  frameWidth = width; 
+  calNum = 0;
 }
 
+void Calibration::calibrate(Mat* inputFrame){
+  for(int i = 0; i < calNum ;i++){
+    cout << kArray[i] << distArray[i] << endl;
+    undistort(*inputFrame, *inputFrame, kArray[i], distArray[i]);
+  }
+}
 
-void Calibration::Calibrate(Mat inputFrame){
-  Mat grayFrame;
-  cvtColor(inputFrame, greyFrame, CV_BGR2GRAY);
+Mat Calibration::calibrate2(Mat inputFrame){
+  Mat output;
+  for(int i = 0; i < calNum ;i++){
+    undistort(inputFrame, output, kArray[i], distArray[i]);
+  }
+  return output;
+}
 
-  cout << "calibrated" << endl;
+void Calibration::calculate(vector<vector<Point3f>>& objectPoints, vector<vector<Point2f>>& imagePoints){
+  Mat k, dist;
+  vector<Mat> rvecs, tvecs;
+  calibrateCamera(objectPoints, imagePoints, Size(frameWidth, frameHeight), k, dist, rvecs, tvecs);
+  kArray.push_back(k);
+  distArray.push_back(dist);
+  calNum++;
+  cout << "calculated!" << endl;
+}
+
+int Calibration::getCalNum(){
+  return calNum;
+}
+
+void Calibration::save(){
+
 }
 
 void showUsage(){
@@ -38,7 +67,7 @@ void showUsage(){
   cout << "Press c to calibration , then saved images in cache are deleted" << endl;
 }
 
-void deleteCache(int* frameNum, vector<Point3f>* objectPoints, vector<Point3f>* imagePoints){
+void deleteCache(int* frameNum, vector<vector<Point3f>>* objectPoints, vector<vector<Point2f>>* imagePoints){
   *frameNum = 0;
   objectPoints->clear();
   imagePoints->clear();
@@ -58,7 +87,7 @@ int main(int argc, char* argv[]){
 	int frameHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
 	cout << frameHeight << frameWidth << endl;
 
-  Calibration C(frameHeight, frameWeight);
+  Calibration C(frameHeight, frameWidth);
 
   vector<Point3f> obj;
 	for(int j=0; j<boardSize.height; j++)
@@ -67,33 +96,42 @@ int main(int argc, char* argv[]){
 	vector<vector<Point3f>> objectPoints;
 	vector<Point2f> oneFramePoints;
 	vector<vector<Point2f>> imagePoints;
+
   showUsage();
+
   int frameNum = 0;
   while(1){
     int key = waitKey(5);
-    Mat inputFrame;
+    Mat inputFrame, imageForView;
     cap >> inputFrame;
 
+    if(C.getCalNum() > 0) inputFrame = C.calibrate2(inputFrame);
+
     bool found = findCirclesGrid(inputFrame, boardSize, oneFramePoints);
-    if (found) drawChessboardCorners(inputFrame, boardSize, Mat(points), found);
-    imshow("captured-image", inputFrame);
+    if (found) drawChessboardCorners(inputFrame, boardSize, Mat(oneFramePoints), found);
+    resize(inputFrame, imageForView, Size(frameWidth/2, frameHeight/2), 0, 0, 0);
+    namedWindow("captured-image", CV_WINDOW_NORMAL);
+    imshow("captured-image", imageForView);
 
     if(key == 113){ //q
       break;
     }
     else if(key == 115 && found){ //s
       frameNum++;
-      //cornerSubPix(InputFrame, oneFramePoints, frameHeight, fmameWeight)
+      Mat gray;
+      cvtColor(inputFrame, gray, CV_BGR2GRAY);
+      cornerSubPix(gray, oneFramePoints, boardSize, Size(frameWidth, frameHeight),
+          TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
       imagePoints.push_back(oneFramePoints);
       objectPoints.push_back(obj);
       cout << frameNum << " frames saved!" << endl;
     }
     else if(key == 99){ //c
-      Calibration.calibrate(objectPoints, imagePoints); 
+      C.calculate(objectPoints, imagePoints); 
       deleteCache(&frameNum, &objectPoints, &imagePoints);
     }
   }
-  Calibration.save();
+  C.save();
   destroyAllWindows();
   return 0;
 }
