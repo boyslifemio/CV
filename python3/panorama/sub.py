af1f37a92d5913c76e496b927cf8353b49b1150f
ryo <ryo.cv.0311@gmail.com>
2017-08-19 22:12:06 +0900

    clean panorama dir

--- python3/panorama/sub.py
+++ python3/panorama/sub.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-import unicodedata
 import copy
 
 class Image:
@@ -17,26 +16,6 @@ class Image:
     def show(self):
         cv2.imshow(self.name, self.image)
         cv2.waitKey(0)
-
-    def resizeMat(self, div):
-        height,width = (self.image).shape[0:2]
-        d = [0,0]
-        if div[0][0] < 0:
-            d[0]=div[0][0]
-            for i in range(-1 * int(div[0][0])):
-                self.image = np.insert(self.image,0,[0,0,0],axis = 1)
-        if div[0][1] > width:
-            for i in range(int(div[0][1]) - width + 3):
-                self.image = np.insert(self.image,-1,[0,0,0],axis = 1)
-        if div[1][0] < 0:
-            d[1]=div[1][0]
-            for i in range(-1 * int(div[1][0])):
-                self.image = np.insert(self.image, 0, np.array((0, 0, 0)), 0)
-        if div[1][1] > height:
-            for i in range(int(div[1][1]) - height + 3):
-                self.image = np.insert(self.image, -1, np.array((0, 0, 0)), 0)
-        print(d)
-        return d
 
     def resizeMat2(self, div):
         height, width = self.image.shape[0:2]
@@ -82,47 +61,6 @@ def calcDst4(H, size):
     div = ((min_x, max_x),(min_y, max_y))
     return div
 
-def SquareCheck(Mat, i, j):
-    flag = 0
-    for col in range(-2,3):
-        if Mat[i+col,j-2].any() and Mat[i+col,j-1].any() and Mat[i+col,j].any() and Mat[i+col,j+1].any() and Mat[i+col,j+2].any():
-            flag+=1
-    if flag == 5:
-        return True
-    else:
-        return False
-
-def MakeCheckMat(blending):
-    check = np.zeros((blending.shape[0], blending.shape[1]), dtype=np.uint8)
-    for i in range(blending.shape[0]):
-        for j in range(blending.shape[1]):
-            if(blending[i][j][0]!=0 or blending[i][j][1]!=0 or blending[i][j][2]!=0):
-                check[i][j] = 255
-    cv2.imshow('check',check)
-
-
-def Write(target, src):
-    print('-----------Writing------------')
-    mask = np.ndarray((target.shape[0], target.shape[1]), dtype=np.uint8)
-    for i in range(src.image.shape[0]):
-        for j in range(src.image.shape[1]):
-            if i < 3 or i > src.image.shape[0]-3 or j < 3 or j > src.image.shape[1]-3:
-                if src.image[i,j].all():
-                    if target[i,j].all():
-                        target[i,j] = src.image[i,j]/2+target[i,j]/2
-                        mask[i,j] = 255
-                    else:
-                        target[i,j] = src.image[i,j]
-            else:
-                if SquareCheck(src.image, i, j):
-                    if SquareCheck(target, i, j):
-                        target[i,j] = src.image[i,j]/2+target[i,j]/2
-                        mask[i,j] = 255
-                    else:
-                        target[i,j] = src.image[i,j]
-    print('-----------Wrote------------')
-    return target, mask
-
 def Write2(target, source, SrcMask):
     mask = cv2.cvtColor(SrcMask,cv2.COLOR_GRAY2RGB)
     target[(mask != [0,0,0])] = source[(mask != [0,0,0])]
@@ -136,36 +74,15 @@ def MakeMask(target, src):
     CommonMaskRGB[(cv2.cvtColor(target,cv2.COLOR_RGB2GRAY) != 0) * (cv2.cvtColor(src,cv2.COLOR_RGB2GRAY) != 0)] = 255
     SrcMaskRGB[(cv2.cvtColor(src,cv2.COLOR_RGB2GRAY) != 0) * (CommonMaskRGB == 0)] = 255
     TargetMaskRGB[(cv2.cvtColor(target,cv2.COLOR_RGB2GRAY) != 0)] = 255
-    cv2.imshow('common',CommonMaskRGB)
-    cv2.imshow('src',SrcMaskRGB)
-    #CommonMask = cv2.cvtColor(CommonMaskRGB,cv2.COLOR_RGB2GRAY)
-    #SrcMask = cv2.cvtColor(SrcMaskRGB,cv2.COLOR_RGB2GRAY)
     CommonMask = cv2.erode(CommonMaskRGB,np.ones((5,5),np.uint8),iterations = 3)
-    #CommonMask = cv2.erode(CommonMask,np.ones((50,50),np.uint8),iterations = 1)
     SrcMask = cv2.dilate(SrcMaskRGB,np.ones((5,5),np.uint8),iterations = 1)
     TargetMask = cv2.dilate(TargetMaskRGB,np.ones((3,3),np.uint8),iterations = 1)
-    #SrcMask = cv2.erode(SrcMask,np.ones((5,5),np.uint8),iterations = 2)
-    #SrcMask = cv2.dilate(SrcMask,np.ones((5,5),np.uint8),iterations = 2)
     return CommonMask, SrcMask, TargetMask
 
 def ArrangeRGB(mat, TargetMask):
     mat[TargetMask==0] = [0,0,0]
     gray = cv2.cvtColor(mat,cv2.COLOR_RGB2GRAY)
     mat[(TargetMask != 0) * (gray == 0)] = 1
-    return mat
-
-def ArrangeHSV(mat, TargetMask, SrcMask):
-    hsv = cv2.cvtColor(mat,cv2.COLOR_RGB2HSV)
-    cv2.imshow('hsv',hsv)
-    h = hsv[:,:,0]
-    s = hsv[:,:,1]
-    v = hsv[:,:,2]
-    #hsv[:,:,1] = 70
-    #hsv[:,:,1] = 255
-    print('---------------')
-    print(s)
-    print(v)
-    mat = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
     return mat
 
 def GetCenter(mask):
@@ -197,14 +114,11 @@ def make_panorama(original1,original2):
     for i in matches:
         if i[0].distance < 500:
             if i[0].distance/i[1].distance < 0.8:
-                print("\U0001F37A", end=' ')
                 goodmatches.append(i[0])
                 querykeys.append((original1.kp[i[0].queryIdx].pt[0],original1.kp[i[0].queryIdx].pt[1]))
                 trainkeys.append((original2.kp[i[0].trainIdx].pt[0],original2.kp[i[0].trainIdx].pt[1]))
 
-    print("\n-----Calculating Homography-----")
     H, status = cv2.findHomography(np.array(trainkeys),np.array(querykeys),cv2.RANSAC, 5.0)
-    print('-----finished to calculate-----')
     div = calcDst4(H, original2.image.shape)
     d = original1.resizeMat2(div)
     T_xy = [[1.0, 0.0, -d[0]],[0.0, 1.0, -d[1]],[0.0, 0.0, 1.0]]
@@ -213,14 +127,7 @@ def make_panorama(original1,original2):
     label = cv2.connectedComponentsWithStats(CommonMask)
     center = np.delete(label[3], 0, 0)
     test = GetCenter(CommonMask)
-    print(center[0])
-    print(test)
-    #MakeCheckMat(panorama)
     blending = cv2.seamlessClone(original1.image, panorama, cv2.cvtColor(CommonMask,cv2.COLOR_GRAY2BGR), (int(test[1]),int(test[0])), cv2.NORMAL_CLONE)
     blending = ArrangeRGB(blending, TargetMask)
     blending = Write2(blending, original1.image, SrcMask)
-    #blending = ArrangeHSV(blending, TargetMask, SrcMask)
-    cv2.imshow('blend', blending)
-    cv2.waitKey(0)
-    print("--next--")
     return blending
